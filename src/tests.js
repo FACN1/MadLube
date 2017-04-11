56232a07cadf14ea84d34e0751c39dd79f467f8a
Matt Lubel <matt_lubel@hotmail.com>
2017-04-11 18:21:14 +0300

    make options clearer

--- src/tests.js
+++ src/tests.js
@@ -2,7 +2,11 @@ const server = require('./server.js');
 const tape = require('tape');
 
 tape('check the home route', (t) => {
-  server.inject({ url: '/', method: 'GET' }, (res) => {
+  const options = {
+    url: '/',
+    method: 'GET'
+  };
+  server.inject(options, (res) => {
     const testString = '<title>Saucy Psychic Ninjas Guesthouse Dinners</title>';
     t.equal(res.statusCode, 200, 'status code should be 200');
     t.ok(res.payload.includes(testString), 'served html should contain the right title');
