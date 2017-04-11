e930cbab3466b9fcf3d0c09f41fc6c4892bc1a27
Matt Lubel <matt_lubel@hotmail.com>
2017-04-11 13:15:49 +0300

    lols

--- src/db_queries.js
+++ src/db_queries.js
@@ -1,7 +1,6 @@
 const fs = require('fs');
 const connPool = require('../database/db_connection.js');
 
-
 var getPosts = function(callback) {
   connPool.query('SELECT * FROM posts', (error, result) => {
     if (error) {
