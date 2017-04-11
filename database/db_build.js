ea117bbe7f32bfbfe1a5b9053cf39d9e69f654ad
Matt Lubel <matt_lubel@hotmail.com>
2017-04-11 16:22:14 +0300

    fix path.join error, adding __dirname

--- database/db_build.js
+++ database/db_build.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 const path = require('path');
 const connPool = require('./db_connection.js');
 
-const sql = fs.readFileSync(path.join('./db_build.sql')).toString();
+const sql = fs.readFileSync(path.join(__dirname, 'db_build.sql')).toString();
 
 connPool.query(sql, (error) => {
   if (error) throw error;
