62575208c6e74d546dc2328a9bf132c7832b92c9
Matt Lubel <matt_lubel@hotmail.com>
2017-04-12 14:35:49 +0300

    add auth to createPost route

--- src/routes/createPost.js
+++ src/routes/createPost.js
@@ -3,6 +3,9 @@ const dbQueries = require('../db_queries');
 module.exports = {
   method: 'post',
   path: '/create-post',
+  config: {
+    auth: 'jwt-strategy'
+  },
   handler: (request, reply) => {
     const data = request.payload;
     dbQueries.createPost(data, (err) => {
