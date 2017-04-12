8bb8d7429a5c503db26f99c8ae5a2bb40e51a352
Matt Lubel <matt_lubel@hotmail.com>
2017-04-12 16:16:29 +0300

    add auth and more context variables to home and add routes

--- src/routes/home.js
+++ src/routes/home.js
@@ -4,11 +4,16 @@ module.exports = {
   method: 'GET',
   path: '/',
   handler: (request, reply) => {
+    // get username and avatar url from cookie
     dbQueries.getPosts((err, postsArray) => {
       if (err) {
         return reply(err);
       }
       const context = {
+        isNotAddPage: true,
+        isAuthenticated: request.auth.isAuthenticated,
+        username: request.auth.credentials ? request.auth.credentials.username : null,
+        avatar_url: request.auth.credentials ? request.auth.credentials.img_url : null,
         posts: postsArray.reverse()
       };
       return reply.view('index', context);
