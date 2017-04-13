6ea6910b01204439f6d43373e6171ae4997ef3dd
Matt Lubel <matt_lubel@hotmail.com>
2017-04-13 12:46:02 +0300

    change create-post route to store correct user

--- src/routes/createPost.js
+++ src/routes/createPost.js
@@ -8,6 +8,12 @@ module.exports = {
   },
   handler: (request, reply) => {
     const data = request.payload;
+    if (request.auth.credentials) {
+      data.user_id = request.auth.credentials.id;
+    } else {
+      // should never really happen, but should be dealt with better
+      data.user_id = 1;
+    }
     dbQueries.createPost(data, (err) => {
       if (err) {
         return reply(err);
