f27e41621f6b1c95082ab2a92afc55aa88be2c09
Matt Lubel <matt_lubel@hotmail.com>
2017-04-13 12:39:08 +0300

    new db query

--- src/db_queries.js
+++ src/db_queries.js
@@ -8,17 +8,6 @@ const createPost = (data, callback) => {
   connPool.query('INSERT INTO posts (dish, user_id, description, chef_name, background_color) VALUES ($1, $2, $3, $4, $5)', [data.dish, 1, data.description, data.chef_name, data.background_color], callback);
 };
 
-const createUser = (userData, callback) => {
-  connPool.query(
-    'INSERT INTO users (username, name) VALUES ($1, $2)',
-    [userData.login, userData.name],
-    (err, result) => {
-      console.log(result.rows[0]);
-      callback(null, result.rows[0].id);
-    }
-  );
-};
-
 const getUserId = (username, callback) => {
   connPool.query(
     'SELECT * FROM users WHERE username=$1',
@@ -28,6 +17,7 @@ const getUserId = (username, callback) => {
         return callback(err);
       }
       if (result.rows.length === 1) {
+        console.log(`user: ${username}, id: ${result.rows[0].id}`);
         return callback(null, result.rows[0].id);
       }
       return callback(null, -1);
@@ -35,9 +25,43 @@ const getUserId = (username, callback) => {
   );
 };
 
+const createUser = (userData, callback) => {
+  // need to add access token as well but need to change db schema for this
+  connPool.query(
+    'INSERT INTO users (username, name) VALUES ($1, $2)',
+    [userData.login, userData.name],
+    (err) => {
+      if (err) {
+        return callback(err);
+      }
+      console.log('new user added: ');
+      // bad as now currently doing 2 db queries
+      return getUserId(userData.login, callback);
+    }
+  );
+};
+
+const getOrCreateUserId = (userData, callback) => {
+  getUserId(userData.login, (err, userId) => {
+    if (err) {
+      callback(err);
+    }
+    if (userId === -1) {
+      return createUser(userData, (createUserErr, newUserId) => {
+        if (createUserErr) {
+          return callback(createUserErr);
+        }
+        return callback(null, newUserId);
+      });
+    }
+    return callback(null, userId);
+  });
+};
+
 module.exports = {
   getPosts,
   createPost,
   createUser,
-  getUserId
+  getUserId,
+  getOrCreateUserId
 };
